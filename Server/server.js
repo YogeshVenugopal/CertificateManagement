c529ddbefacc75f30683be1cb2032734881879ab
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-03-08 20:54:28 +0530

    Import essential packages and Config the database

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import pool from "./Config/db.js";
 
 dotenv.config();
 
@@ -11,3 +12,11 @@ app.use(cors());
 app.listen(process.env.PORT,() => 
     console.log(`The port is running on ${process.env.PORT}`)
 )
+
+pool.query('SELECT NOW()', (err, res) => {
+    if (err) {
+        console.error('Error connecting to the database', err);
+    } else {
+        console.log('Database connection successful:', res.rows[0]);
+    }
+});
