9024572579d3d3d6991d1987ad6d1af2779db242
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2026-03-08 19:51:10 +0530

    Base Setup

---
+++ Server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.listen(process.env.PORT,() => 
+    console.log(`The port is running on ${process.env.PORT}`)
+)
